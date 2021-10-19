767c51bea75a13b561c3e98e71c3baef88194a1a
bhagyaqw <92821708+bhagyaqw@users.noreply.github.com>
2021-10-20 01:35:16 +0530

    added more data

--- detail-audio.js
+++ detail-audio.js
@@ -2,3 +2,19 @@ define(['domReady', 'iscroll'], function(domReady, myScroll) {
   console.log('Loading detail audio.');
 
 });
+define(['domReady', 'iscroll'], function(domReady, myScroll) {
+  console.log('Loading detail audio.');
+
+});
+define(['domReady', 'iscroll'], function(domReady, myScroll) {
+  console.log('Loading detail audio.');
+
+});
+define(['domReady', 'iscroll'], function(domReady, myScroll) {
+  console.log('Loading detail audio.');
+
+});
+define(['domReady', 'iscroll'], function(domReady, myScroll) {
+  console.log('Loading detail audio.');
+
+});
